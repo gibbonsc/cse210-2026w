cd167323ff137234801a4762907d3e67f07fd000
C Gibbons <gibbonsc@byui.edu>
2026-03-10 13:18:03 -0600

    RoundShape polymorphism demo

---
+++ sandbox/RoundShapes/Circle.cs
@@ -0,0 +1,12 @@
+class Circle : RoundShape
+{
+    protected double _radius;
+
+    public Circle(double r)
+    {
+        _radius = r;
+    }
+    // void SetRadius(double r) { _radius = r; }
+
+    public override double Area() { return Math.PI * _radius * _radius; }
+}
---
+++ sandbox/RoundShapes/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace RoundShapes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<RoundShape> rolling_stones = new List<RoundShape>();
+        rolling_stones.Add( new Circle(2.0));
+        rolling_stones.Add( new Cylinder(10.0, 2.0));
+        rolling_stones.Add( new Sphere(500));
+        foreach (RoundShape shape in rolling_stones)
+        {
+            Console.WriteLine($"Area of shape: {shape.Area()}");
+        }
+
+        // Circle steve = new Circle(2.0);
+        // Cylinder mike = new Cylinder(10.0, 2.0);
+        // Sphere sully = new Sphere(500);
+        // Console.WriteLine($"Area of steve: {steve.Area()}");
+        // Console.WriteLine($"Area of mike: {mike.Area()}");
+        // Console.WriteLine($"Area of sully: {sully.Area()}");
+    }
+}
